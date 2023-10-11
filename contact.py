a6b00e8845d08d6debc833d9abfab39107e288e5
Vsevolod Kulakov <Sev7715@yandex.ru>
2023-10-11 16:31:24 +0300

    Создан тест для создания контакта, с использованием вспомогательных методов и вспомогательного класса

---
+++ contact.py
@@ -0,0 +1,10 @@
+
+class Contact:
+    def __init__(self, name, middlename, lastname, email, group):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.email = email
+        self.group = group
+
+
