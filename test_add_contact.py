ee93f61f6178ab5485b62fdac153d68849d04c93
Vsevolod Kulakov <Sev7715@yandex.ru>
2023-10-12 14:27:05 +0300

    вынесена фикстура в отдельный класс, переведено на pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,97 +1,22 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from contact import Contact
-
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_contact(self):
-        self.login("admin", "secret")
-        self.create_contact(Contact("name", "middlename",
-                                        "lastname", "email"))
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.XPATH, "//body").click()
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-
-    def return_to_contacts_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "home page").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        self.open_contacts_page(wd)
-        # fill contact form
-        wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(contact.name)
-        wd.find_element(By.NAME, "middlename").click()
-        wd.find_element(By.NAME, "middlename").clear()
-        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
-        wd.find_element(By.NAME, "lastname").click()
-        wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
-        wd.find_element(By.NAME, "email").click()
-        wd.find_element(By.NAME, "email").clear()
-        wd.find_element(By.NAME, "email").send_keys(contact.email)
-        wd.find_element(By.NAME, "new_group").click()
-        wd.find_element(By.XPATH, "//option[@value='[none]']").click()
-        # submit contact creation
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
-        self.return_to_contacts_page()
+from application import Application
 
 
-    def open_contacts_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "add new").click()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page(wd)
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+def test_add_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact("name", "middlename",
+                                    "lastname", "email"))
+    app.logout()
 
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+
+
     
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
