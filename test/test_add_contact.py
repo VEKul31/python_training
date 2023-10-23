c0b14f188f539a23b15881d01a6e03d1759696ca
Vsevolod Kulakov <Sev7715@yandex.ru>
2023-10-23 12:56:43 +0300

    Параметризация тестов для создания контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,38 @@
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = ([Contact(name="", lastname="",
+                     address="", email="", email2="", email3="",
+                     homephone="", mobilephone="", workphone="", secondaryphone="")] +
+            [
+                Contact(name=random_string("name", 10), lastname=random_string("lastname", 10),
+                        address=random_string("address", 10), email=random_string("email", 10),
+                        mobilephone=random_string("mobilephone", 10), workphone=random_string("workphone", 10))
+                for i in range(2)
+            ])
+
+testdata2 = [
+    Contact(name=name, lastname=lastname, address=address, email=email, mobilephone=mobilephone, workphone=workphone)
+    for name in ["", random_string("name", 10)]
+    for lastname in ["", random_string("lastname", 10)]
+    for address in ["", random_string("address", 10)]
+    for email in ["", random_string("email", 10)]
+    for mobilephone in ["", random_string("mobilephone", 10)]
+    for workphone in ["", random_string("workphone", 10)]
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = (Contact("name", "middlename",  "lastname",
-                                   address="почтовый адрес", email="123", email2="234", email3="345",
-                                   homephone="111", mobilephone="222", workphone="333", secondaryphone="444"))
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
