289fa52365e112c4bf4ca944acf54c526006d812
Vsevolod Kulakov <Sev7715@yandex.ru>
2023-10-29 16:00:19 +0300

    Отношения между объектами

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,13 +1,14 @@
 import pymysql.cursors
 # from fixture.db import DbFixture
 from fixture.orm import ORMFixture
+from model.group import Group
 
 # connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 # db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id="108"))
     for item in l:
         print(item)
     print(len(l))
